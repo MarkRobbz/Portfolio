e277758e9eb9ea9903adf8ef7744142216817318
40267145 <markrobb259@gmail.com>
2020-09-29 09:40:48 +0100

    Fixed navigatio for mobile screens by implamenting a java query to fix it.

---
+++ interactions.js
@@ -0,0 +1,28 @@
+
+window.addEventListener('resize', resized);
+
+function openNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    //element.style.removeProperty("display");
+    //moves the red box to side need fixed tomorrow. 
+    nav.style.width = "55%";
+}
+
+function closeNav() {
+    var nav = document.getElementsByTagName("nav")[0];
+    //element.style.display = "none";
+    nav.style.width = "0";
+}
+
+
+function resized(){
+    var nav = document.getElementsByTagName("nav")[0];
+    if (document.documentElement.clientWidth > 700){
+        nav.style.width = "15%";
+    } else if( document.documentElement.clientWidth < 700){
+        nav.style.width = "0";
+    }
+}
+
+
+
