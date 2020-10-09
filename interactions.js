32b6b00fac511e5f963f3f558d57ea7f4ff7ff2a
40267145 <markrobb259@gmail.com>
2020-10-09 15:29:33 +0100

    got the nav highlight working, just need theme nav now and tidy code.

--- interactions.js
+++ interactions.js
@@ -14,17 +14,32 @@ function closeNav() {
 }
 
 
-function resized(){
+function resized(){  //Prevents nav colapsing to wrong size.
     var nav = document.getElementsByTagName("nav")[0];
-    if (document.documentElement.clientWidth > 700){
+    if (document.documentElement.clientWidth > 725){
         nav.style.width = "15%";
-    } else if( document.documentElement.clientWidth < 700){
+    } else if( document.documentElement.clientWidth < 725){
         nav.style.width = "0";
     }
 }
 
 
-function navScroll(id) {
-    var elmnt = document.getElementById(id);
-    elmnt.scrollIntoView({ block: 'start',  behavior: 'smooth' });
-  }
+
+
+$(document).ready(function() {
+    $('#jNav').onePageNav({
+        currentClass: 'current',
+        changeHash: false,
+        scrollSpeed: 400,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: ''
+    });
+    
+    $('.do').click(function(e) {
+      $('#section-4').append('<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>');
+      e.preventDefault();
+    });
+    
+  });
+
