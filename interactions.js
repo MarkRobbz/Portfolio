906fb71c372074593e52cb0d263484346a83b50d
40267145 <markrobb259@gmail.com>
2021-12-23 14:34:59 +0000

    changes to center linkedin box/ reload screen .

--- interactions.js
+++ interactions.js
@@ -16,8 +16,10 @@ function resized() { //Prevents nav being wrong size when closed.
     var nav = document.getElementsByTagName("nav")[0];
     if (document.documentElement.clientWidth > 725) {
         nav.style.width = "15%";
+        document.location.reload(true);
     } else if (document.documentElement.clientWidth < 725) {
         nav.style.width = "0";
+        document.location.reload(true);
     }
 }
 
